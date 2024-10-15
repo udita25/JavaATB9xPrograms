c52ef42f75afbd92fb6d1933c54c206aaa2e9f54
udita <udita2509@gmail.com>
2024-10-16 01:25:30 +0530

    Commit and push

---
+++ src/Oct/ex10102024/Lab005.java
@@ -0,0 +1,16 @@
+package Oct.ex10102024;
+
+public class Lab005 {
+    public  static void main (String [] args){
+        int a=5;
+        float b=3.41f;
+        char c='A';
+        String name="Udita";
+        boolean d= true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(name);
+        System.out.println(d);
+    }
+}
