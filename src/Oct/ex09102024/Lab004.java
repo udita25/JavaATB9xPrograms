b8dc4ac891e3a303a77ed548e34ae8e89163503f
udita <udita2509@gmail.com>
2024-10-16 00:19:35 +0530

    Commit and push

---
+++ src/Oct/ex09102024/Lab004.java
@@ -0,0 +1,19 @@
+package Oct.ex09102024;
+
+
+
+public class Lab004 {
+    public static void main(String[] args){
+        int num= 5;
+        System.out.printf("%d x %d = %d", num, 1, num*1);
+        System.out.printf("\n%d x %d = %d", num, 2, num*2);
+        System.out.printf("\n%d x %d = %d", num, 3, num*3);
+        System.out.printf("\n%d x %d = %d", num, 4, num*4);
+        System.out.printf("\n%d x %d = %d", num, 5, num*5);
+        System.out.printf("\n%d x %d = %d", num, 6, num*6);
+        System.out.printf("\n%d x %d = %d", num, 7, num*7);
+        System.out.printf("\n%d x %d = %d", num, 8, num*8);
+        System.out.printf("\n%d x %d = %d", num, 9, num*9);
+        System.out.printf("\n%d x %d = %d", num, 10, num*10);
+    }
+}
