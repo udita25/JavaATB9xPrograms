bf88cf114c1fd5dcb609013d9bfc7ad894cb9bb8
udita <udita2509@gmail.com>
2024-11-17 19:14:20 +0530

    Commit and push

---
+++ src/Oct/ex01112024/Dog.java
@@ -0,0 +1,44 @@
+package Oct.ex01112024;
+
+class Dog {
+    private String name;
+    private int age;
+    private String breed;
+    private String color;
+    private double weight;
+
+    // Constructor
+    public Dog(String name, int age, String breed, String color, double weight) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    // Getters and setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getBreed() { return breed; }
+    public void setBreed(String breed) { this.breed = breed; }
+
+    public String getColor() { return color; }
+    public void setColor(String color) { this.color = color; }
+
+    public double getWeight() { return weight; }
+    public void setWeight(double weight) { this.weight = weight; }
+
+    // Method to display dog information
+    public void displayInfo() {
+        System.out.println("\nDog Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age + " years");
+        System.out.println("Breed: " + breed);
+        System.out.println("Color: " + color);
+        System.out.println("Weight: " + weight + " kg");
+    }
+}
---
+++ src/Oct/ex01112024/Lab024.java
@@ -0,0 +1,19 @@
+package Oct.ex01112024;
+
+public class Lab024 {
+    public static void main(String[] args){
+        Dog dog1 = new Dog("Max", 3, "Labrador", "Golden", 30.5);
+        Dog dog2 = new Dog("Luna", 2, "Husky", "White", 25.0);
+        Dog dog3 = new Dog("Rocky", 4, "German Shepherd", "Black and Tan", 32.0);
+        Dog dog4 = new Dog("Rex", 6, "Husky", "White",28.0);
+        Dog dog5 = new Dog("Zeus", 4, "Belgian Malinois", "Brown", 34.0);
+
+        // Displaying information for all dogs
+        dog1.displayInfo();
+        dog2.displayInfo();
+        dog3.displayInfo();
+        dog4.displayInfo();
+        dog5.displayInfo();
+
+    }
+}
