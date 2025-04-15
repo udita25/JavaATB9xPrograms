66b6edde71879b3cebacbba4405d7d2d53094489
udita <udita2509@gmail.com>
2025-04-15 23:36:57 +0530

    Commit and push

--- src/PatternExercise/HalfPyramid.java
+++ src/PatternExercise/HalfPyramid.java
@@ -12,7 +12,7 @@ public class HalfPyramid {
 
         for(int i = 1; i <= num; i++){
             for(int j = 1; j <= i; j++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
         }
